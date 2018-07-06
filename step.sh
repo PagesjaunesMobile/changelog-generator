0f0e0f9787929578d389af3ed3b13dc9ecb44c5c
CyrilPatrice <cpatrice@pagesjaunes.fr>
2018-07-06 15:11:56 +0200

    feat(CHANGELOG): point d'origine des commits

--- step.sh
+++ step.sh
@@ -14,7 +14,7 @@ if [ "$TAG_DEST" = "HEAD" ]; then
   if [[ "$BITRISE_GIT_BRANCH" =~ (develop|master|release) ]]; then   
     previousTag=$(git rev-list --parents HEAD | head -1| cut -d' ' -f2)
   else
-    previousTag=$(diff -u <(git rev-list --first-parent HEAD) <(git rev-list --first-parent $BITRISEIO_GIT_BRANCH_DEST) | sed -ne 's/^ //p' | head -1) 
+    previousTag=$(diff -u <(git rev-list --first-parent HEAD) <(git rev-list --first-parent "origin/${BITRISEIO_GIT_BRANCH_DEST}") | sed -ne 's/^ //p' | head -1) 
   fi
   
   if [ -n "$tag_head" ]; then
